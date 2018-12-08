3cab4d284b46caaec9277bbd28a07a099436b932
caoqiqiang <caoqiqiang@fangdd.com>
2018-12-09 01:00:06 +0800

    init choodon mybaits generator project

---
+++ src/main/java/com/choodon/mybaits/generator/ChoodonShellRunner.java
@@ -0,0 +1,15 @@
+package com.choodon.mybaits.generator;
+
+import org.mybatis.generator.api.ShellRunner;
+
+/**
+ * ChoodonShellRunner
+ *
+ * @author michael
+ * @since 2018-12-08
+ */
+public class ChoodonShellRunner {
+    public static void main(String[] args) {
+        ShellRunner.main(args);
+    }
+}
---
+++ src/main/java/com/choodon/mybaits/generator/plugin/OverrideIsMergeablePlugin.java
@@ -0,0 +1,33 @@
+package com.choodon.mybaits.generator.plugin;
+
+import org.mybatis.generator.api.GeneratedXmlFile;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+/**
+ * OverrideIsMergeablePlugin
+ *
+ * @author michael
+ * @since 2018-12-09
+ */
+public class OverrideIsMergeablePlugin extends PluginAdapter {
+
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    @Override
+    public boolean sqlMapGenerated(GeneratedXmlFile sqlMap, IntrospectedTable introspectedTable) {
+        try {
+            Field field = sqlMap.getClass().getDeclaredField("isMergeable");
+            field.setAccessible(true);
+            field.setBoolean(sqlMap, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+}
