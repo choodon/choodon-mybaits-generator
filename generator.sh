72b7aebf28343d37308a18a725dc481fc35b72a2
choodon <769872108@qq.com>
2018-12-11 01:16:52 +0800

    add and optimized generatorConfig.xml file

---
+++ generator.sh
@@ -0,0 +1,1 @@
+java -jar choodon-mybaits-generator.jar -configfile generatorConfig.xml -overwrite
--- src/main/java/com/choodon/mybaits/generator/comment/ChoodonCommentGenerator.java
+++ src/main/java/com/choodon/mybaits/generator/comment/ChoodonCommentGenerator.java
@@ -16,11 +16,12 @@ import java.util.Properties;
  * @since 2018-12-08
  */
 public class ChoodonCommentGenerator extends DefaultCommentGenerator {
-
+    @Override
     public void addConfigurationProperties(Properties properties) {
 
     }
 
+    @Override
     public void addFieldComment(Field field,
                                 IntrospectedTable introspectedTable,
                                 IntrospectedColumn introspectedColumn) {
@@ -49,10 +50,12 @@ public class ChoodonCommentGenerator extends DefaultCommentGenerator {
         field.addJavaDocLine(" */"); //$NON-NLS-1$
     }
 
+    @Override
     public void addFieldComment(Field field, IntrospectedTable introspectedTable) {
 
     }
 
+    @Override
     public void addModelClassComment(TopLevelClass topLevelClass,
                                      IntrospectedTable introspectedTable) {
 
@@ -80,38 +83,47 @@ public class ChoodonCommentGenerator extends DefaultCommentGenerator {
         topLevelClass.addJavaDocLine(" */"); //$NON-NLS-1$
     }
 
+    @Override
     public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable) {
 
     }
 
+    @Override
     public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable, boolean markAsDoNotDelete) {
 
     }
 
+    @Override
     public void addEnumComment(InnerEnum innerEnum, IntrospectedTable introspectedTable) {
 
     }
 
+    @Override
     public void addGetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
 
     }
 
+    @Override
     public void addSetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
 
     }
 
+    @Override
     public void addGeneralMethodComment(Method method, IntrospectedTable introspectedTable) {
 
     }
 
+    @Override
     public void addJavaFileComment(CompilationUnit compilationUnit) {
 
     }
 
+    @Override
     public void addComment(XmlElement xmlElement) {
 
     }
 
+    @Override
     public void addRootComment(XmlElement rootElement) {
 
     }
